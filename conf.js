c843485684cc88b69ae355adc4e51d3bda42e657
Jim Schubert <james.schubert@gmail.com>
2011-09-08 17:44:44 -0400

    oath dance and a simple test

---
+++ conf.js
@@ -0,0 +1,5 @@
+module.exports = exports = {
+    consumerKey: 'iTL3cuTg2Qmd7atJWxDxQA',
+    consumerSecret: 'fiNO4sSIGYnBPhZ15veLSFpgKhaSp40i4yORFQTyYI',
+    oauthCallback: 'http://127.0.0.1:8080/auth/twitter/callback'
+};
