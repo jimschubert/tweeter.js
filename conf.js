727145db6363de8c3dffa731cafd7680c60ddb13
Jim Schubert <james.schubert@gmail.com>
2011-09-18 21:44:47 -0400

    removed cached conf.js

--- conf.js
+++
@@ -1,5 +0,0 @@
-module.exports = exports = {
-    consumerKey: 'ENTER_YOUR_CONSUMER_KEY_HERE',
-    consumerSecret: 'ENTER_YOUR_CONSUMER_SECRET_HERE',
-    oauthCallback: 'http://127.0.0.1:8080/auth/twitter/callback'
-};
