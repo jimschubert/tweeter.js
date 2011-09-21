e80db793ff59255382ae573644d27565cd24af25
Jim Schubert <james.schubert@gmail.com>
2011-09-20 21:17:03 -0400

    Switched submodules for npm installed node_modules

--- test/tweeter.test.js
+++ test/tweeter.test.js
@@ -6,8 +6,7 @@ try {
     var p = require('sys').puts;
     p("Cannot find nodeunit module.");
     p("You can download submodules for this project by doing:\n");
-    p("\tgit submodule init");
-    p("\tgit submodule update\n");
+    p("\tnpm install -d\n");
     process.exit();
 }
 
